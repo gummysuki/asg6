04a18afd17c73bedd9b361b1562f8eacf2e594f3
gummysuki <maninmao13@gmail.com>
2026-04-07 11:49:12 +0700

    Update userRoutes import path in app.js

--- sonarqube_proj/app.js
+++ sonarqube_proj/app.js
@@ -1,11 +1,7 @@
 const express = require("express");
-const userRoutes = require("./routes/user.routes");
-
+const userRoutes = require("./src/routes/user.routes");
 const app = express();
-
 app.use(express.json());
 app.disable("x-powered-by");
-
 app.use("/api/users", userRoutes);
-
 module.exports = app;
