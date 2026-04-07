08bc9cf8cbfe6c377728bddcadfa4fbec012509a
gummysuki <maninmao13@gmail.com>
2026-04-07 11:53:22 +0700

    Update app.js

--- sonarqube_proj/app.js
+++ sonarqube_proj/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const userRoutes = require("./src/routes/user.routes");
+const userRoutes = require("./routes/user.routes");
 const app = express();
 app.use(express.json());
 app.disable("x-powered-by");
